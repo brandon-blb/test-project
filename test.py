f3ffa3e5ee2fd15ead315d362de2b068cf9b329a
Brandon <brandon@sudotech.co.za>
2023-11-27 15:35:21 +0200

    [IMP] Sort by surname after name

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class TestProject(models.Model):
 
     _name = 'test.project'
     _description = 'Test Project'
-    _order = 'name'
+    _order = 'name, surname'
 
     name = fields.Char(string="Name")
     surname = fields.Char(string="Surname")
