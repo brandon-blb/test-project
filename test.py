573df3564478061d0154ef10110acc777d5af71f
Brandon <brandon@sudotech.co.za>
2023-11-27 15:45:03 +0200

    [IMP] Spacing Fix

--- test.py
+++ test.py
@@ -13,3 +13,4 @@ class TestProject(models.Model):
     name = fields.Char(string="Name")
     surname = fields.Char(string="Surname")
     id_number = fields.Char(string="ID Number", required=True, copy=False)
+    
