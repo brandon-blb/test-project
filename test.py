b1fa2dcb4b5b98f2930b8f03465573b5c0e93893
Brandon <brandon@sudotech.co.za>
2023-11-27 15:20:39 +0200

    [INIT]

---
+++ test.py
@@ -0,0 +1,14 @@
+from odoo import xyz
+
+from odoo import models, fields, api, exceptions, tools, _
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class TestProject(models.Model):
+
+    _name = 'test.project'
+    _description = 'Test Project'
+
+    name = fields.Char(string="Name")
+    surname = fields.Char(string="Surname")
